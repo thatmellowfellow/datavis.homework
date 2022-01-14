ff40e950480195162ae131b9c0451e2c04eddc97
Martin Manerov <manerov97@yandex.ru>
2022-01-15 02:27:00 +0300

    fix #2

--- js/index.js
+++ js/index.js
@@ -209,7 +209,7 @@ loadData().then(data => {
             var pickedCountry = d3.select(this)
                 .attr('country');
             d3.selectAll('circle')
-                .attr('stroke-width', 'default');
+                .attr('stroke-width', 'initial');
             d3.selectAll('circle')
                 .filter(d => d.country == pickedCountry)
                 .attr('stroke-width', 4)
